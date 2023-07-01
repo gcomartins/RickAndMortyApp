e73ed26371cda872d6eb1b4984c01b6b9f648e78
Guilherme Carneiro <guilherme.martin@valemobi.com.br>
2023-07-01 13:31:29 -0300

    Update CharactersViewModel.kt

--- app/src/main/java/com/example/rickandmortyapp/ui/personagens/view_model/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/personagens/view_model/CharactersViewModel.kt
@@ -17,10 +17,8 @@ class CharactersViewModel: ViewModel() {
     private val retrofit = Rest.getInstance()
     val service = retrofit.create(PersonagensService::class.java)
 
-    private val _character = MutableLiveData<Personagem>()
     private val _characters = MutableLiveData<List<Personagem>>()
 
-    var character: LiveData<Personagem> = _character
     val characters: LiveData<List<Personagem>> = _characters
 
     var currentPage: Int = 1
@@ -46,24 +44,6 @@ class CharactersViewModel: ViewModel() {
             }
 
             override fun onFailure(call: Call<GetAllPersonagensResponse>, t: Throwable) {
-                Log.e("PersonagemService   ", "Erro ao buscar o personagem:" + t.message)
-            }
-        })
-    }
-
-    fun getCharacterById(id: Int){
-        val call = service.getCharacterById(id)
-        call.enqueue(object : Callback<Personagem> {
-            override fun onResponse(
-                call: Call<Personagem>,
-                response: Response<Personagem>
-            ) {
-                if (response.isSuccessful) {
-                    _character.value = response.body()
-                }
-            }
-
-            override fun onFailure(call: Call<Personagem>, t: Throwable) {
                 Log.e("PersonagemService   ", "Erro ao buscar o personagem:" + t.message)
             }
         })
